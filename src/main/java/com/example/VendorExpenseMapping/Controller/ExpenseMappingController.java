57f895c587c73eaa2220c5cecb31a6c72b3b4fda
Amsavarthani Selvaraj <s.amsavarthani@gmail.com>
2025-04-29 14:55:41 +0530

    second commit

--- src/main/java/com/example/VendorExpenseMapping/Controller/ExpenseMappingController.java
+++ src/main/java/com/example/VendorExpenseMapping/Controller/ExpenseMappingController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,4 +39,14 @@ public class ExpenseMappingController {
 		return expenseMappingService.getAllField(keyword);
 	}
 	
+	@PutMapping("/update/{id}")
+	public ExpenseMapping update(@PathVariable int id,@RequestBody ExpenseMapping newData) {
+		return expenseMappingService.update(id , newData);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String delete(@PathVariable int id) {
+		expenseMappingService.deleteMapping(id);
+		return " Deleted Successfully ";
+	}
 }
--- src/main/java/com/example/VendorExpenseMapping/Service/ExpenseMappingService.java
+++ src/main/java/com/example/VendorExpenseMapping/Service/ExpenseMappingService.java
@@ -6,14 +6,24 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.VendorExpenseMapping.Entity.Expense;
 import com.example.VendorExpenseMapping.Entity.ExpenseMapping;
+import com.example.VendorExpenseMapping.Entity.Vendor;
 import com.example.VendorExpenseMapping.Repository.ExpenseMappingRepository;
+import com.example.VendorExpenseMapping.Repository.ExpenseRepository;
+import com.example.VendorExpenseMapping.Repository.VendorRepository;
 
 @Service
 public class ExpenseMappingService {
 
 	@Autowired
 	private ExpenseMappingRepository expenseMappingRepository;
+	
+	@Autowired
+	private VendorRepository vendorRepository;
+	
+	@Autowired
+	private ExpenseRepository expenseRepository;
 	
 	public ExpenseMapping createMapping(ExpenseMapping expenseMapping) {
 		return expenseMappingRepository.save(expenseMapping);
@@ -25,5 +35,26 @@ public class ExpenseMappingService {
 
 	public List<ExpenseMapping> getAllField(String keyword) {
 		return expenseMappingRepository.searchAllFields(keyword);
+	}
+	
+	public ExpenseMapping update(int id, ExpenseMapping newData) {
+		ExpenseMapping e =  expenseMappingRepository.findById(id).orElseThrow(() -> new RuntimeException("Expense Mapping Id Not found"));
+		
+		if(e != null) {
+			int vendorId=(newData.getVendor().getId());
+			int expenseId=(newData.getExpense().getId());
+			
+			Vendor vendor=vendorRepository.findById(vendorId).orElseThrow(()-> new RuntimeException(" VendorId not found"));
+			Expense expense=expenseRepository.findById(expenseId).orElseThrow(()-> new RuntimeException("ExpenseId not found"));
+			e.setVendor(vendor);
+			e.setExpense(expense);
+			
+			return expenseMappingRepository.save(e);
+		}
+      throw new RuntimeException("Not Found");
+}
+	
+	public void deleteMapping(int id) {
+		expenseMappingRepository.deleteById(id);
 	}
 }
